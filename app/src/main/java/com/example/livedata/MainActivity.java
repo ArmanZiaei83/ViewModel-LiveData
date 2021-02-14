0dee0b6a1fd5e57d18281019e96de41abd73b9e2
armanziaei <armanziaei83@gmaio.com>
2021-02-15 00:46:38 +0330

    Feat UI Components : ui created & matched to MainActivity

--- app/src/main/java/com/example/livedata/MainActivity.java
+++ app/src/main/java/com/example/livedata/MainActivity.java
@@ -1,14 +1,32 @@
 package com.example.livedata;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModel;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button addNumberBtn;
+    TextView numberTextView;
+
+    ViewModel viewModel;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        addNumberBtn = findViewById(R.id.addBtn);
+        numberTextView = findViewById(R.id.num);
+
+        addNumberBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                                
+            }
+        });
     }
 }
